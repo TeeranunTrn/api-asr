f4d59531757fd08773a326b3bdae390adb31b5d7
TeeranunTrn <89390180+TeeranunTrn@users.noreply.github.com>
2022-04-13 23:50:34 +0700

    Add files via upload

---
+++ try2.py
@@ -0,0 +1,6 @@
+import subprocess
+
+result = subprocess.run(
+            ['ffmpeg', '-i', './ten.mp4', '-ac', '1', '-ar', '16000', './ten.mp3'],
+            stdout=subprocess.PIPE, shell=True
+        )
